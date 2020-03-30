f02426f71e2ccbf12cc1c690e74568d25a475698
huijuan.zhou <huijuan.zhou@greedyint.cn>
2020-03-30 18:23:51 +0800

    更新

--- src/config.js
+++
@@ -1,5 +0,0 @@
-export default {
-    apiConfig: {
-        baseUrl: "http://localhost:3355"
-    }
-}
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'src/styles/home.less';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          xixixihahah
+      </header>
+    </div>
+  );
+}
+
+export default App;
